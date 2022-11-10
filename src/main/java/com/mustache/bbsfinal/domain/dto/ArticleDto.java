e847f4915d8df43e5012c2a7020856819aad3488
momineee <p.rainmakerm@gmail.com>
2022-11-10 23:57:34 +0900

    ArticleDto - id도 받을 수 있도록 설정

--- src/main/java/com/mustache/bbsfinal/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbsfinal/domain/dto/ArticleDto.java
@@ -3,13 +3,17 @@ package com.mustache.bbsfinal.domain.dto;
 import com.mustache.bbsfinal.domain.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class ArticleDto {
+    private Long id;
     private String title;
     private String content;
 
     public Article toEntitiy() {
-       return new Article(title, content);
+       return new Article(this.id, this.title, this.content);
     }
 }
