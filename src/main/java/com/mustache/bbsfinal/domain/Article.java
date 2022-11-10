580f306ca7421e2e2480551a897a0b746c23ba55
momineee <p.rainmakerm@gmail.com>
2022-11-10 11:27:36 +0900

    Article - DB insert 위한 article Entitiy 생성

---
+++ src/main/java/com/mustache/bbsfinal/domain/Article.java
@@ -0,0 +1,24 @@
+package com.mustache.bbsfinal.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "article")
+@NoArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
