4d991df16680c22ece51485eee07004a0b72cbb5
momineee <p.rainmakerm@gmail.com>
2022-11-11 11:50:21 +0900

    Hospital entity 생성

---
+++ src/main/java/com/mustache/bbsfinal/domain/Hospital.java
@@ -0,0 +1,25 @@
+package com.mustache.bbsfinal.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "nation_wide_hospitals")
+@Getter
+public class Hospital {
+    @Id // generatedValue 쓰지 않는다. 입력을 해놓았기 때문
+    private Integer id;
+
+    @Column(name = "road_name_address")
+    private String roadNameAddress;
+
+    @Column
+    private String hospitalName;
+    private Integer patientRoomCount;
+    private Integer totalNumberOfBeds;
+    private Float totalAreaSize;
+}
