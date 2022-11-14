6ab2c8afc8efd004ae9a799257168dfc1e9103f0
momineee <p.rainmakerm@gmail.com>
2022-11-14 13:45:40 +0900

    HospitalRepository & Test case

--- src/main/java/com/mustache/bbsfinal/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbsfinal/repository/HospitalRepository.java
@@ -3,5 +3,9 @@ package com.mustache.bbsfinal.repository;
 import com.mustache.bbsfinal.domain.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+
 }
--- src/test/java/com/mustache/bbsfinal/repository/HospitalRepositoryTest.java
+++ src/test/java/com/mustache/bbsfinal/repository/HospitalRepositoryTest.java
@@ -1,25 +1,41 @@
 package com.mustache.bbsfinal.repository;
 
 import com.mustache.bbsfinal.domain.Hospital;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-public class HospitalRepositoryTest {
+class HospitalRepositoryTest {
 
     @Autowired
     HospitalRepository hospitalRepository;
 
     @Test
-    void name() {
+    void findById(){
         Optional<Hospital> hospital = hospitalRepository.findById(1);
-        Hospital hp = hospital.get();
+        Hospital  hp = hospital.get();
         System.out.println(hp.getId());
-        assertEquals(1, hp.getId());
+        assertEquals(1,hp.getId());
+    }
+
+    @Test
+    @DisplayName("BusinessTypeName이 보건소 보건지소 보건진료소인 데이터가 잘 나오는지")
+    void findByBusinessTypeNameIn() {
+        List<String> inClues = new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건지소");
+        inClues.add("보건진료소");
+        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
+        for (var hospital :
+                hospitals) {
+            System.out.println(hospital.getHospitalName());
+        }
     }
 }
