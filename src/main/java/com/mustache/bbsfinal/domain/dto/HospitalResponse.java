4a7a8796eac0ee6734f85e86aeb4a2341c3def76
momineee <p.rainmakerm@gmail.com>
2022-11-16 16:26:15 +0900

    HospitalResponse 수정

--- src/main/java/com/mustache/bbsfinal/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbsfinal/domain/dto/HospitalResponse.java
@@ -1,4 +1,20 @@
 package com.mustache.bbsfinal.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class HospitalResponse {
+    private Integer id;
+    private String roadNameAddress;
+    private String hospitalName;
+    private Integer patientRoomCount;
+    private Integer totalNumberOfBeds;
+    private String businessTypeName;
+    private Float totalAreaSize;
 }
